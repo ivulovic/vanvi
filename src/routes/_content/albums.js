04b3367f826ee392a9281f2cfd4580018e676e3d
Ivan Vulovic <ivanv@Ivans-MacBook-Pro.local>
2022-05-04 21:12:16 +0200

    updated content

--- src/routes/_content/albums.js
+++ src/routes/_content/albums.js
@@ -7,9 +7,15 @@ export const albums = [
         slug: 'garage',
         name: 'Garage',
         performer: 'VANVI',
-        image: getImage('stars.jpg'),
+        image: getImage('garage.jpg'),
         tracks: [
-            {id: '', fileName: getAudio('stars/vanvi_spring_morning.mp3'), title: 'Spring Morning', timestamp: 1651603878040 },
+            {id: '1', fileName: getAudio('garage/vanvi_soundtrack_1.mp3'), title: 'SOUNDTRACK 1', timestamp: 1651603878040 },
+            {id: '2', fileName: getAudio('garage/vanvi_soundtrack_2.mp3'), title: 'SOUNDTRACK 2', timestamp: 1651603878040 },
+            {id: '3', fileName: getAudio('garage/vanvi_soundtrack_3.mp3'), title: 'SOUNDTRACK 3', timestamp: 1651603878040 },
+            {id: '4', fileName: getAudio('garage/vanvi_soundtrack_4.mp3'), title: 'SOUNDTRACK 4', timestamp: 1651603878040 },
+            {id: '5', fileName: getAudio('garage/vanvi_soundtrack_5.mp3'), title: 'SOUNDTRACK 5', timestamp: 1651603878040 },
+            {id: '6', fileName: getAudio('garage/vanvi_soundtrack_6.mp3'), title: 'SOUNDTRACK 6', timestamp: 1651603878040 },
+            {id: '7', fileName: getAudio('garage/vanvi_soundtrack_7.mp3'), title: 'SOUNDTRACK 7', timestamp: 1651603878040 },
         ]
     }
 ]
